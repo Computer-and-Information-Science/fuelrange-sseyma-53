91452d76631eff5c0b28a2b91e867d4f40c6028b
sseyma-53 <sseyma5317@gmail.com>
2025-09-09 19:56:38 +0000

    coded how many miles can be driven on the remaining fuel

--- main.cpp
+++ main.cpp
@@ -2,14 +2,30 @@
 // TODO: YOUR NAME HERE
 #include <iostream>
 // TODO: Add the "using" statement
-
+using namespace std;
 int main() {
   // TODO: Declare your variables, with comments for each
+  double tankCapacity;
+  double mpg;
+  double tankFilled; 
 
   // TODO: Prompt for and input the required values
+  cout << "Please input the capacity of your fueld tank: ";
+  cin >> tankCapacity;
+
+  cout << "Please input the mpg of your fuel tank: ";
+  cin >> mpg;
+
+  cout << "Please input the percent full: ";
+  cin >> tankFilled;
 
   // TODO: Calculate the results
+  double remainingFuel = (tankFilled*tankCapacity)/100;
+  double milesLeft = remainingFuel*mpg;
 
   // TODO: Output the results
   
+  cout << "The vehicle can be driven " << milesLeft << " miles on the remaining fuel.";
+
+
 }
